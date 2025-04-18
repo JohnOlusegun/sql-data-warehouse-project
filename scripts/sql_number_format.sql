c0897fac080313af79aba46530245d84be844318
John Olusegun <138987087+JohnOlusegun@users.noreply.github.com>
2025-04-18 11:17:32 +0100

    Update and rename sql_number_format to sql_number_format.sql

--- scripts/sql_number_format.sql
+++ scripts/sql_number_format.sql
@@ -4,65 +4,47 @@ SELECT
     'N' AS FormatType, 
     FORMAT(1234.56, 'N') AS FormattedValue, 
     'Numeric_Default' AS Description
-
 UNION ALL
-
 SELECT 
     'P' AS FormatType, 
     FORMAT(1234.56, 'P') AS FormattedValue, 
     'Percentage'
-
 UNION ALL
-
 SELECT 
     'C' AS FormatType, 
     FORMAT(1234.56, 'C') AS FormattedValue, 
     'Currency'
-
 UNION ALL
-
 SELECT 
     'E' AS FormatType, 
     FORMAT(1234.56, 'E') AS FormattedValue, 
     'Scientific_Notation'
-
 UNION ALL
-
 SELECT 
     'F' AS FormatType, 
     FORMAT(1234.56, 'F') AS FormattedValue, 
     'Fixed_point'
-
 UNION ALL
-
 SELECT 
     'N0' AS FormatType, 
     FORMAT(1234.56, 'N0') AS FormattedValue, 
     'Numeric no decimals'
-
 UNION ALL
-
 SELECT 
     'N1' AS FormatType, 
     FORMAT(1234.56, 'N1') AS FormattedValue, 
     'Numeric one Decimal'
-
 UNION ALL
-
 SELECT 
     'N2' AS FormatType, 
     FORMAT(1234.56, 'N2') AS FormattedValue, 
     'Numeric two Decimals'
-
 UNION ALL
-
 SELECT 
     'N_de-DE' AS FormatType, 
     FORMAT(1234.56, 'N', 'de-DE') AS FormattedValue, 
     'Numeric German format'
-
 UNION ALL
-
 SELECT 
     'N_en-US' AS FormatType, 
     FORMAT(1234.56, 'N', 'en-US') AS FormattedValue, 
